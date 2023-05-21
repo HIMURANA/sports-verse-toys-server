b389e52a2ef3d3c44a1559abc984c27c264193eb
soheldot <soheldot1239@gmail.com>
2023-05-21 11:10:02 +0600

    add get for toy data losd

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require ('cors')
-const { MongoClient, ServerApiVersion} = require('mongodb');
-const  ObjectID = require('mongodb').ObjectId;
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -47,7 +46,18 @@ async function run() {
         const result = await cursor.toArray()
         res.send(result)
     })
+    app.get('/toys', async(req, res)=>{
+      const cursor = toyCollection.find().limit(20)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
 
+    app.get('/toys/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollection.findOne(query)
+      res.send(result)
+    })
     app.post('/toys', async(req, res)=>{
       const newToy = req.body
       const result = await toyCollection.insertOne(newToy)
