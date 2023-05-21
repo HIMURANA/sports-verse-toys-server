cddc1a028ee00280fc5e0dce49c9db9a3893b2bb
soheldot <soheldot1239@gmail.com>
2023-05-21 15:39:31 +0600

    add put for update toy data

--- index.js
+++ index.js
@@ -69,6 +69,22 @@ async function run() {
       res.send(result)
     })
 
+    app.put('/toys/:id', async(req, res)=>{
+      const id  = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const option = {upsert: true}
+      const updateToy = req.body
+      const toys ={
+        $set:{
+          price:updateToy.price,
+          quantity:updateToy.quantity,
+          description:updateToy.description
+        }
+      }
+      const result = await toyCollection.updateOne(filter,toys, option)
+      res.send(result)
+    })
+
     app.delete('/toys/:id', async(req, res)=>{
       const id = req.params.id
       const query ={_id: new ObjectId(id)}
